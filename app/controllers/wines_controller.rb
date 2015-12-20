14bfa2622d3a037a2e8ad1d54550b138f4078e7c
Michael Williams <mwilli23@masonlive.gmu.edu>
2015-12-20 13:42:10 -0800

    Added Edit  New Wine

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -16,6 +16,17 @@ class WinesController < ApplicationController
   redirect_to @wine
  end
 
+def edit
+  @wine = Wine.find(params[:id])
+end
+
+def update
+  @wine = Wine.find(params[:id])
+  @wine.update(wine_params)
+  redirect_to @wine
+end
+
+
  private
 
  def wine_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "/wines/new" => "wines#new", as: 'new_wine'
   get "/wines/:id" => "wines#show", as: 'wine'
   post "/wines" => "wines#create"
+  get "/wines/:id/edit" => "wines#edit", as: 'edit_wine'
+  patch "/wines/:id" => "wines#update"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
